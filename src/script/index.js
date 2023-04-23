91422026a02da729183864b1baa854baab859be5
jeancsm94 <jean0king@gmail.com>
2023-04-23 15:50:46 -0300

    Ajustes rotas

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,4 @@
+const production = false;
 function checkDevice () {
     if (navigator.userAgent.match(/Android/i) ||  navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
         return true;
@@ -7,10 +8,17 @@ function checkDevice () {
 }
 
 const isMobi = checkDevice();
-
-if (isMobi) {
-    window.location.replace("/src/page/mobile/");
-}else {
-    window.location.replace("/src/page/desktop/");
+if (!production) {
+    if (isMobi) {
+        window.location.replace("/src/page/mobile/");
+    }else {
+        window.location.replace("/src/page/desktop/");
+    }
+} else {
+    if (isMobi) {
+        window.location.replace("IEPE.github.io/src/page/mobile/");
+    }else {
+        window.location.replace("IEPE.github.io/src/page/desktop/");
+    }
 }
 
