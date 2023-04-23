66ba297508cbf24c1907c13a107bc136e1cd6908
Jean Carlos dos Santos Maidana <jean0king@gmail.com>
2023-04-23 15:55:16 -0300

    Ajustes enviroment

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,4 @@
-const production = false;
+const production = true;
 function checkDevice () {
     if (navigator.userAgent.match(/Android/i) ||  navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
         return true;
